80d428007903d0ca91f11481c2b8f4631d1282f4
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-08-26 11:29:17 -0700

    Add computed property

--- app/components/q-tile.js
+++ app/components/q-tile.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  numberOfAnswers:
+  Ember.computed('q.answers', function() {
+    return this.get('q.answers.length');
+    })
 });
