bf2b7ee776b9dddb1440dae6f9b5d4718fb0cf50
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-08-26 14:41:52 -0700

    Add service

--- app/components/q-details.js
+++ app/components/q-details.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  clipBoard: Ember.inject.service(),
   showUpdate: false,
   actions:{
     showUpdate() {
@@ -13,6 +14,9 @@ export default Ember.Component.extend({
 
     update() {
       this.set("showUpdate", false);
+    },
+    addToClipboard(item) {
+      this.get('clipBoard').add(item);
     }
   }
 });
